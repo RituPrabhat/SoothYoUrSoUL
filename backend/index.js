94f2da12bc82d88ba63b9b9e2c6e4bce85850764
Ritu Prabhat <rtprbht@gmail.com>
2025-08-21 23:55:14 +0530

    api routes clear

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: [process.env.FRONTEND_URL || 'http://localhost:5173', 'http://localhost:5173', 'http://localhost:3000'],
+  origin: ["https://soothyoursoul.vercel.app", "http://localhost:5173"],
   credentials: true
 }));
 
